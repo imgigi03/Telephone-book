8bed0946a0698acbf4449f61b3da1306b5dab9ae
imgigi03 <luigicontaldo2003@gmail.com>
2025-06-11 10:29:44 +0200

    Add files via upload

---
+++ ProgettoGruppo20-master/src/test/java/it/unisa/diem/softeng/progettogruppo20/Struttura/TelefonoTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.diem.softeng.progettogruppo20.Struttura;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author luigicocco
+ */
+public class TelefonoTest {
+
+    /**
+     * Test of aggiungiDato method, of class Telefono.
+     */
+    @Test
+    public void testAggiungiDato() {
+
+        System.out.println("aggiungiDato");
+        String tel1 = "118";
+        String tel2 = "111333"; 
+        String tel3 = "333111555"; 
+        String tel4 = "333111555a"; 
+        
+        Telefono telefono = new Telefono();
+        telefono.aggiungiDato(tel1);
+        telefono.aggiungiDato(tel2);
+        assertEquals(telefono.getDati().get(0), tel1); //test dell'invariante
+        telefono.aggiungiDato(tel3);
+        assertEquals(telefono.getDati().get(0), tel1); //test dell'invariante
+        telefono.aggiungiDato(tel4);
+        assertEquals(telefono.getDati().get(0), tel1); //test dell'invariante
+        assertEquals(telefono.getDati().get(2), tel3); //test dell'invariante
+        
+        assertEquals(true, telefono.contiene(tel1));
+        assertEquals(false, telefono.contiene(tel2)); //in questo modo viene testato anche il metodo contiene()
+        assertEquals(true, telefono.contiene(tel3));
+        assertEquals(false, telefono.contiene(tel4));    
+        
+
+    }
+
+}
---
+++ ProgettoGruppo20-master/src/test/java/it/unisa/diem/softeng/progettogruppo20/gestione/ListaContattiTest.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.diem.softeng.progettogruppo20.gestione;
+
+import it.unisa.diem.softeng.progettogruppo20.Struttura.Contatto;
+import java.util.Set;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Contaldo Luigi, Cocco Luigi, D'Errico Fabrizio, D'Onofrio Matteo
+ */
+public class ListaContattiTest {
+
+    private ListaContatti lista;
+
+    /**
+     * Test of aggiungiContatto method, of class ListaContatti.
+     */
+    @Test
+    public void testAggiungiContatto() {
+        lista = new ListaContatti();
+        lista.aggiungiContatto("Luigi", "Cocco", "1234", "4567", "7890", "luigi@gmail.com", "cocco@gmail.com", "luigicocco@gmail.com");
+
+        Contatto c = new Contatto("Luigi", "Cocco", "1234", "4567", "7890", "luigi@gmail.com", "cocco@gmail.com", "luigicocco@gmail.com");
+
+        assertTrue(lista.getElenco().contains(c));
+        
+        lista.getElenco().clear();
+    }
+
+    /**
+     * Test of cercaContatto method, of class ListaContatti.
+     */
+    @Test
+    public void testCercaContatto() {
+        lista = new ListaContatti();
+        lista.aggiungiContatto("Fabrizio", "D'Errico", "1756", "9879", "1111", "fabrizio@gmail.com", "derrico@gmail.com", "fabder@gmail.com");
+
+        Contatto c = new Contatto("Fabrizio", "D'Errico", "1756", "9879", "1111", "fabrizio@gmail.com", "derrico@gmail.com", "fabder@gmail.com");
+
+        assertTrue(lista.cercaContatto("Fab", "D").getElenco().contains(c));
+        assertEquals(1, lista.getElenco().size());
+        
+        for(Contatto cont : lista.getElenco()) {  //test dell'invariante
+            assertTrue(cont.equals(c));
+        }
+        
+        lista.getElenco().clear();
+    }
+
+    /**
+     * Test of rimuoviContatto method, of class ListaContatti.
+     */
+    @Test
+    public void testRimuoviContatto() {
+        lista = new ListaContatti();
+        Contatto c = new Contatto("Fabrizio", "D'Errico", "1756", "9879", "1111", "fabrizio@gmail.com", "derrico@gmail.com", "fabder@gmail.com");
+
+        lista.rimuoviContatto(c);
+
+        assertFalse(lista.cercaContatto("Fab", "D").getElenco().contains(c));
+        
+        lista.getElenco().clear();
+    }
+
+    /**
+     * Test of modificaContatto method, of class ListaContatti.
+     */
+    @Test
+    public void testModificaContatto() {
+
+        lista = new ListaContatti();
+        Contatto c = new Contatto("Fabrizio", "D'Errico", "175655555", "988145879", "111111111", "fabrizio@gmail.com", "derrico@gmail.com", "fabder@gmail.com");
+
+        lista.aggiungiContatto("Fabrizio", "D'Errico", "175655555", "988145879", "111111111", "fabrizio@gmail.com", "derrico@gmail.com", "fabder@gmail.com");
+
+        assertTrue(lista.modificaContatto(c, "Matteo", "D'Onofrio", "124675009", "", "", "matteo@gmail.com", "", ""));
+
+        assertEquals(1, lista.getElenco().size()); //test dell'invariante
+        
+        lista.getElenco().clear();
+    }
+
+}
